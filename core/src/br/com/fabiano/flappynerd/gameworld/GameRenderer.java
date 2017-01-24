a2509e2bbb0f2242352470a08bcb67645138a593
Fabiano <fabianoyoschitaki@gmail.com>
2017-01-24 01:54:48 -0200

    Cap. 11 terminado.

--- core/src/br/com/fabiano/flappynerd/gameworld/GameRenderer.java
+++ core/src/br/com/fabiano/flappynerd/gameworld/GameRenderer.java
@@ -243,6 +243,7 @@ public class GameRenderer {
     }
 
     private void drawScore() {
+        Gdx.app.log("GameRenderer", "drawScore");
         int length = ("" + myWorld.getScore()).length();
         AssetLoader.shadow.draw(batcher, "" + myWorld.getScore(),
                 68 - (3 * length), midPointY - 82);
@@ -263,7 +264,6 @@ public class GameRenderer {
 
         }
     }
-
 
     private void showRedCollisionRectangles() {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
